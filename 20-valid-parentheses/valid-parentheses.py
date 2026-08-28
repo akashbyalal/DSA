3508e92f889774451e18a216e77b493701f311af
Akash Byalal <akashbyalal02@gmail.com>
2026-08-28 13:41:52 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (63.32%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,17 +1,12 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        
         if len(s) % 2 != 0: return False
-
-        hashMap = {")":"(", "}":"{", "]": "["}
         stack = []
+        d = { ")" : "(", "}": "{", "]":"["}
 
-        for i in range(len(s)):
-            if s[i] in hashMap:
-                if stack and stack[-1] == hashMap[s[i]]:
-                    stack.pop()
-                else: return False
-            else: stack.append(s[i])
-        
-        return False if stack else True
-
+        for i in s:
+            if i in d:
+                if stack and stack[-1] == d[i]: stack.pop()
+                else : return False
+            else: stack.append(i)
+        return True if not stack else False
