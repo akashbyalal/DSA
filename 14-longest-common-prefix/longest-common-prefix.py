a0267d301cf42b2bc97de1a79506601635e6a960
Akash Byalal <akashbyalal02@gmail.com>
2026-08-28 13:26:01 +0530

    Time: 4 ms (7.51%) | Memory: 19.2 MB (73.63%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs: return ""
+
+        str = ""
+
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i >= len(s) or s[i] != strs[0][i]:
+                    return str
+            str += strs[0][i]
+        return str
